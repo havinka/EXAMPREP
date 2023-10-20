77578ce4a309caee89437dd0da8491ae4a90ef7d
havinka <havsmollova@icloud.com>
2023-10-20 20:47:45 +0300

    update

---
+++ JobOffer.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+public abstract class JobOffer
+{
+    private string jobTitle;
+
+    private string company;
+
+    private double salary;
+
+    protected JobOffer(string jobTitle, string company, double salary)
+    {
+        this.jobTitle = jobTitle;
+        this.company = company;
+        this.salary = salary;
+    }
+
+    public string JobTitle
+    {
+        get
+        {
+            return this.jobTitle;
+        }
+        private set
+        {
+            if (value.Length < 3 || value.Length>30)
+            {
+                throw new ArgumentException("JobTitle should be between 3 and 30 characters!");
+            }
+
+            this.jobTitle = value;
+        }
+    }
+
+    public string Company
+    {
+        get
+        {
+            return this.company;
+        }
+        private set
+        {
+            if (value.Length < 3 || value.Length>30)
+            {
+                throw new ArgumentException("Company should be between 3 and 30 characters! ");
+            }
+            this.company = value;
+        }
+    }
+
+    public double Salary
+    {
+        get { return this.salary; }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Salary should be 0 or positive! ");
+            }
+            this.salary = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Job Title: {JobTitle} ");
+        sb.AppendLine($"Company: {Company} ");
+        sb.AppendLine($"Salary: {Salary:f2} BGN ");
+
+        return sb.ToString().TrimEnd();
+    }
+
+}
+
+
---
+++ OnSiteJobOffer.cs
@@ -0,0 +1,29 @@
+﻿
+public class OnSiteJobOffer : JobOffer
+{
+    private string city;
+
+    public string City
+    {
+        get { return this.city; }
+        set
+        {
+            if (value.Length < 3 || value.Length>30)
+            {
+                throw new ArgumentException("Error");
+            }
+            city = value;
+        }
+    }
+    public OnSiteJobOffer(string jobTitle, string company, double salary,string city) : base(jobTitle, company, salary)
+    {
+        City = city;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() +$"{Environment.NewLine}City: {City}";
+    }
+}
+
+
